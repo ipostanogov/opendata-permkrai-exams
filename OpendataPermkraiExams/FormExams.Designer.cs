feeae13acd9d95bd89e5ecc9effd2cde89d75da7
Igor Postanogov <ipostanogov@outlook.com>
2016-02-18 21:29:25 +0500

    Initial commit

---
+++ OpendataPermkraiExams/FormExams.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace OpendataPermkraiExams
+{
+    partial class FormExams
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chrtMain = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.trbYear = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.chrtMain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbYear)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chrtMain
+            // 
+            chartArea2.AxisX.Interval = 1D;
+            chartArea2.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea2.AxisY.Title = "Доля лиц (%)";
+            chartArea2.Name = "ChartArea1";
+            this.chrtMain.ChartAreas.Add(chartArea2);
+            this.chrtMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.chrtMain.Legends.Add(legend2);
+            this.chrtMain.Location = new System.Drawing.Point(0, 0);
+            this.chrtMain.Name = "chrtMain";
+            this.chrtMain.Size = new System.Drawing.Size(284, 217);
+            this.chrtMain.TabIndex = 0;
+            this.chrtMain.Text = "chrtMain";
+            // 
+            // trbYear
+            // 
+            this.trbYear.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.trbYear.Location = new System.Drawing.Point(0, 217);
+            this.trbYear.Name = "trbYear";
+            this.trbYear.Size = new System.Drawing.Size(284, 45);
+            this.trbYear.TabIndex = 1;
+            // 
+            // FormExams
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.chrtMain);
+            this.Controls.Add(this.trbYear);
+            this.Name = "FormExams";
+            this.Text = "Пример о результатах ЕГЭ @ opendata.permkrai.ru";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.chrtMain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbYear)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chrtMain;
+        private System.Windows.Forms.TrackBar trbYear;
+    }
+}
---
+++ OpendataPermkraiExams/FormExams.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.FileIO;
+
+namespace OpendataPermkraiExams
+{
+    public partial class FormExams : Form
+    {
+        private readonly Dictionary<int, Series> _serieses = new Dictionary<int, Series>();
+
+        public FormExams()
+        {
+            InitializeComponent();
+            LoadData();
+            chrtMain.Titles.Add(
+                "Доля лиц, сдавших единый государственный экзамен по русскому языку и математике, в общей численности выпускников муниципальных общеобразовательных учреждений, участвовавших в едином государственном экзамене по данным предметам");
+            if (_serieses.Count > 0)
+            {
+                var seriesKvp = _serieses.First();
+                trbYear.Minimum = _serieses.Keys.Min();
+                trbYear.Maximum = _serieses.Keys.Max();
+                trbYear.Value = seriesKvp.Key;
+                chrtMain.Series.Add(seriesKvp.Value);
+                trbYear.ValueChanged += trbYear_ValueChanged;
+            }
+        }
+
+        private void LoadData()
+        {
+            var request =
+                WebRequest.Create(
+                    "http://opendata.permkrai.ru/LoadDataManager/api/getcsv.ashx?factor=14825&cube=6707330");
+            using (var response = request.GetResponse())
+            using (var dataStream = response.GetResponseStream())
+            using (var parser = new TextFieldParser(dataStream, Encoding.GetEncoding("Windows-1251")))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(";");
+                var isFirstRow = true;
+                var dateColumnIdx = -1;
+                var okatoColumnIdx = -1;
+                var valueColumnIdx = -1;
+                var locations = new List<string>();
+                while (!parser.EndOfData)
+                {
+                    var fields = parser.ReadFields().ToList();
+                    if (isFirstRow)
+                    {
+                        dateColumnIdx = fields.IndexOf("Дата");
+                        okatoColumnIdx = fields.IndexOf("ОКАТО");
+                        valueColumnIdx = fields.IndexOf("Значение");
+                        isFirstRow = false;
+                    }
+                    else
+                    {
+                        var year = int.Parse(Strings.Right(fields[dateColumnIdx], 4));
+                        var value = double.Parse(fields[valueColumnIdx]);
+                        var location = fields[okatoColumnIdx];
+                        if (!locations.Contains(location))
+                            locations.Add(location);
+                        var thisLocationIdx = locations.IndexOf(location);
+                        var dataPoint = new DataPoint(thisLocationIdx, value)
+                        {
+                            AxisLabel = location
+                        };
+                        if (!_serieses.ContainsKey(year))
+                        {
+                            _serieses[year] = new Series(year + " год");
+                        }
+                        _serieses[year].Points.Add(dataPoint);
+                    }
+                }
+            }
+        }
+
+        private void trbYear_ValueChanged(object sender, EventArgs e)
+        {
+            chrtMain.Series.Clear();
+            if (_serieses.ContainsKey(trbYear.Value))
+                chrtMain.Series.Add(_serieses[trbYear.Value]);
+        }
+    }
+}
